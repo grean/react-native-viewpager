409785c3b8df6169ac70254cc700092207f42d40
Réan Guillaume <rean.guillaume@gmail.com>
2021-05-26 22:03:55 +0200

    yarn workspaces

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,5 +1,12 @@
-const config = require('@grean/react-native-metro-config-app-example/index.js');
-console.log(config)
+let config = null;
+try {
+  //Use this script when your are in a monorepo yarn workspaces
+  config = require('../../../metro.config');
+} catch (ex) {
+  config = require('@grean/react-native-metro-config-app-example/index.js');
+}
+
+// console.log(config)
 
 module.exports = config
 
